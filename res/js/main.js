188d91668f5995a70dfe34a493361e07b1ff91aa
Matějíček <matejkonsel3@seznam.cz>
2024-01-23 10:58:26 +0100

    update seznamu ucitelu

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,9 @@
-
+const teachers = document.getElementsByClassName("teachers")[0];
 
 //JSON - javascriptova objektova notace
 
 const loadData = async () => {
-    const file = await fetch("./res/data/data.json");
+    const file = await fetch("./res/data/teachers.json");
     const data = await file.json();
     return data;
 }
@@ -15,7 +15,19 @@ window.onload = async () => {
     /*data.forEach((joke)=> {
         console.log(joke.content)
     })*/
-    data.map((joke)=>{
-        console.log(joke.content)
+/*      data.map((joke)=>{
+        console.log(joke.content);
+    })                     */
+    data.map((teacher) => {
+        teachers.innerHTML += `
+    <div class="teacher">
+        <h1 class="teacher-heading">${teacher.title}${teacher.name}</h1>
+        <p class="teacher-age">Věk:${teacher.age} </p>
+        <p class="teacher-hair">Vlasy: ${teacher.hair}</p>
+        <p class="teacher-characteristics">Charakteristika: ${teacher.characteristics}</p>
+        <p class="teacher-subjects">Učí: ${teacher.subjects}</p>
+    </div>
+        `;
     })
+
 }
